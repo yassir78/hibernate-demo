f0afa89a2e8b5d5c130a71fa1ead1cc5b6ca7aac
Yassir acaf <yassir.acaf@gmail.com>
2025-01-18 19:41:47 +0100

    comparison expressions

--- src/main/java/org/chaosmaker/App.java
+++ src/main/java/org/chaosmaker/App.java
@@ -1,15 +1,16 @@
 package org.chaosmaker;
 
 import jakarta.persistence.*;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.ParameterExpression;
-import jakarta.persistence.criteria.Root;
+import jakarta.persistence.criteria.*;
 import org.chaosmaker.helpers.EntityFactoryBuilder;
 import org.chaosmaker.interceptors.AuditLogInterceptor;
+import org.chaosmaker.models.AuctionType;
+import org.chaosmaker.models.Bid;
 import org.chaosmaker.models.Item;
+import org.chaosmaker.models.User;
 import org.hibernate.Session;
 
+import java.math.BigDecimal;
 import java.util.*;
 
 /**
@@ -28,27 +29,93 @@ public class App {
         transaction.rollback();
         try {
             transaction.begin();
-
             CriteriaBuilder cb = em.getCriteriaBuilder();
             CriteriaQuery criteria = cb.createQuery(Item.class);
 
-            ParameterExpression<String> itemNameParameter =
-                    cb.parameter(String.class);
-
-            Root<Item> i = criteria.from(Item.class);
-            Query query = em.createQuery(
-                    criteria.select(i).where(
-                            cb.equal(
-                                    i.get("name"),
-                                    itemNameParameter
-                            )
+            // select b from Bid b where b.amount > 100
+            Root<Bid> b = criteria.from(Bid.class);
+            criteria.select(b).where(
+                    cb.gt(
+                            b.<BigDecimal>get("amount"),
+                            new BigDecimal("100")
                     )
-            ).setParameter(itemNameParameter, "value");
+            );
+            // select u from User u where u.username in ('johndoe', 'janeroe')
+            Root<User> u = criteria.from(User.class);
+            criteria.select(u).where(
+                    cb.in(u.<String>get("username"))
+                            .value("johndoe")
+                            .value("janeroe")
+            );
 
-            query.setFirstResult(40).setMaxResults(10);
+            // select i from Item i
+            // where i.auctionType = org.chaosmaker.models.AuctionType
+            Root<Item> i = criteria.from(Item.class);
+            criteria.select(i).where(
+                    cb.equal(
+                            i.<AuctionType>get("auctionType"),
+                            AuctionType.HIGHEST_BID
+                    )
+            );
+            // select i from Item i where i.buyNowPrice is null
+            Root<Item> i1 = criteria.from(Item.class);
+            criteria.select(i).where(
+                    cb.isNull(i.get("buyNowPrice"))
+            );
+            // select i from Item i where i.buyNowPrice is not null
+            Root<Item> i2 = criteria.from(Item.class);
+            criteria.select(i).where(
+                    cb.isNotNull(i.get("buyNowPrice"))
+            );
+            // select u from User u where u.username like 'john%'
+            Root<User> u1 = criteria.from(User.class);
+            criteria.select(u).where(
+                    cb.like(u.get("username"), "john%")
+            );
+            // select u from User u where u.username not like 'john%'
+            Root<User> u2 = criteria.from(User.class);
+            criteria.select(u).where(
+                    cb.like(u.get("username"), "john%").not()
+            );
+            // select u from User u where u.username like '%oe%'
+            Root<User> u3 = criteria.from(User.class);
+            criteria.select(u).where(
+                    cb.like(u.<String>get("username"), "%oe%")
+            );
+            // select i from Item i
+            // where i.name like 'Name\_with\_underscores' escape :escapeChar
+            //query.setParameter("escapeChar", "\\");
+            Root<Item> i3 = criteria.from(Item.class);
+            criteria.select(i).where(
+                    cb.like(i.get("name"), "Name\\_with\\_underscores", '\\')
+            );
 
-            List<Item> items = query.getResultList();
+            // select b from Bid b where (b.amount / 2) - 0.5 > 49
+            Root<Bid> b1 = criteria.from(Bid.class);
+            criteria.select(b).where(
+                    cb.gt(
+                            cb.diff(
+                                    cb.quot(b.<BigDecimal>get("amount"), 2),
+                                    0.5
+                            ),
+                            49
+                    )
+            );
+            // select i from Item i
+            // where (i.name like 'Fo%' and i.buyNowPrice is not null)
+            // or i.name = 'Bar'/
+            Root<Item> i4 = criteria.from(Item.class);
+            Predicate predicate = cb.and(
+                    cb.like(i.<String>get("name"), "Fo%"),
+                    cb.isNotNull(i.get("buyNowPrice"))
+            );
 
+            predicate = cb.or(
+                    predicate,
+                    cb.equal(i.<String>get("name"), "Bar")
+            );
+
+            criteria.select(i).where(predicate);
 
 
         } catch (Exception ex) {
---
+++ src/main/java/org/chaosmaker/models/AuctionType.java
@@ -0,0 +1,7 @@
+package org.chaosmaker.models;
+
+public enum AuctionType {
+    HIGHEST_BID,
+    LOWEST_BID,
+    FIXED_PRICE
+}
